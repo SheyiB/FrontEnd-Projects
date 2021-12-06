4f62b8fbee25a25673c499aa197d5897251d1d02
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2021-12-06 01:03:58 +0100

    Live entry table update

--- script/liventry.js
+++ script/liventry.js
@@ -5,6 +5,7 @@ const dob = document.querySelector('.dob');
 const phonenumber = document.querySelector('.number');
 const email = document.querySelector('.mail');
 const password = document.querySelector('.password');
+const tableSample = document.querySelector('.sample');
 
 
 console.log(firstname.value)
@@ -14,16 +15,34 @@ console.log(firstname.value)
 // }
 
 function logi(){
-    console.log(
-        {
-            "firstname" : firstname.value,
-            "lastname" : lastname.value,
-            "midname" : midname.value,
-            "dob" : dob.value,
-            "phone number" : phonenumber.value,
-            "email" : email.value,
-            "password" : password.value
-        }
-    )
+const newt = tableSample.cloneNode(true)
+
+const n = 7
+for (let index = 0; index < 7; index++) {
+    //const element = [index];
+    const liste = [firstname.value,lastname.value,midname.value,dob.value,phonenumber.value,email.value,password.value]
+    newt.getElementsByTagName('td')[index].innerText = liste[index]
 
 }
+
+
+
+document.querySelector('.table').append(newt)
+
+    // console.log(
+    //     {
+
+
+    //         "firstname" : firstname.value,
+    //         "lastname" : lastname.value,
+    //         "midname" : midname.value,
+    //         "dob" : dob.value,
+    //         "phone number" : phonenumber.value,
+    //         "email" : email.value,
+    //         "password" : password.value
+
+    //     }
+    // )
+
+}
+
